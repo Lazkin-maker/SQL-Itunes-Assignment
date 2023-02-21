015a15060a3230969ea5193dc0d01ff471eb7dfb
Lazkin Youssuf <lazkin.youssuf96@yahoo.com>
2023-02-21 15:17:34 +0100

    refactor : added base crud repository

---
+++ TestProjectSQL/Repositories/ICrudRepository.cs
@@ -0,0 +1,18 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectSQL.Repositories
+{
+    public interface ICrudRepository<T, Id>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(Id id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(Id id);
+    }
+}
+
